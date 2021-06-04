5057859c7c661564f2fe6ca4c173560f46071adf
ZebG <>
2021-06-04 03:17:57 +0000

    Asked candidate for their name. Displayed a welcome using input name. Defined variables: question, correctAnswer, candidateAnswer. Asked a question and provided feedback.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ('');
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = ('1) Who was the first American woman in space? ');
+let correctAnswer = ('Sally Ride');
+let candidateAnswer = ('');
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,19 +15,22 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+const input = require('readline-sync');
+candidateName = input.question("Candidate, please enter your first and last name: ");
+console.log('');
 }
-
+console.log('');
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+const input = require('readline-sync');
+candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+console.log('Your Answer: ' + candidateAnswer);
+console.log('Correct Answer: Sally Ride')
 
   let grade;
   
@@ -38,7 +41,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log('Welcome astronaut candidate ' + candidateName +' to your initial evaluation.');
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
