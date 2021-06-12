78d8fe71a7f83b97c9ba4530569638fba6155a9c
ZebG <84686042+ZebG44@users.noreply.github.com>
2021-06-12 01:25:13 +0000

    Added five questions to the quiz and recorded user answers to an array.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,13 +5,14 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = ('');
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ('1) Who was the first American woman in space? ');
-let correctAnswer = ('Sally Ride');
+let question = ('1) Who was the first American woman in space? ');let correctAnswer = ('Sally Ride');
 let candidateAnswer = ('');
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ['1) Who was the first American woman in space?',
+'2) True or False: 5 kilometers == 5000 meters?',
+'3) (5+3)/2 * 10 = ?',
+'4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?','5) What is the minimum crew size for the ISS?']; 
+let correctAnswers = ['Sally Ride', 'true','40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -23,18 +24,22 @@ console.log('');
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 const input = require('readline-sync');
-candidateAnswer = input.question(question);
+for (let i = 0; i < questions.length; i++) {
+  console.log(questions[i]);
+   (candidateAnswer = input.question('Your Answer: '));
+    (candidateAnswers.push(candidateAnswer));
+console.log(`Correct Answer: ${correctAnswers[i]}`);
+console.log();
+}
+
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log('Your Answer: ' + candidateAnswer);
-console.log('Correct Answer: Sally Ride')
 
   let grade;
   
-
   return grade;
 }
 
@@ -42,6 +47,7 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   console.log('Welcome astronaut candidate ' + candidateName +' to your initial evaluation.');
+  console.log();
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
